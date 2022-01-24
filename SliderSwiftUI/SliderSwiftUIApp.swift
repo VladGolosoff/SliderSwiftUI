9c07b5062ec53e40f0a4c5749298745fb23e1754
VladGolosoff <v.golosoff@gmail.com>
2022-01-24 23:50:35 +0300

    Initial Commit

---
+++ SliderSwiftUI/SliderSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderSwiftUIApp.swift
+//  SliderSwiftUI
+//
+//  Created by Владислав Голосов on 24.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct SliderSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
